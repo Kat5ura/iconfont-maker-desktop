0bc08f10d1e3a00a4ab6b58c6e5dd758730bb5dd
Kat5ura <971969499@qq.com>
2017-10-10 11:28:38 +0800

    1. modify layout 2. modify Title

--- src/renderer/pages/AdvanceMaker.vue
+++ src/renderer/pages/AdvanceMaker.vue
@@ -2,21 +2,37 @@
   .advance-maker {
     padding: 20px;
 
-    .svg-list {
-      margin: 20px 0;
-      padding: 5px 0;
-      line-height: 1;
-      border-top: dashed 1px #8eb2ff;
+    .icons-wrapper{
 
-      & * {
-        box-sizing: border-box;
+      .el-form-item__content{
+        display: flex;
+        max-height: 150px;
+        overflow: overlay;
+        flex-wrap: wrap;
+
+        &>div:first-child{
+          display: inline-block;
+        }
       }
+
+      .el-upload.el-upload--text{
+        .el-button{
+          display: inline-block;
+          width: 50px;
+          height: 50px;
+          border-radius: 25px;
+        }
+      }
+
       .svg-list-item {
         position: relative;
         display: inline-block;
         padding: 15px;
         width: 50px;
         height: 50px;
+        margin-left: 5px;
+        border: 1px solid silver;
+        border-radius: 25px;
 
         img {
           display: block;
@@ -41,15 +57,22 @@
           color: #ff4c4c;
           background-color: rgba(0, 0, 0, .8);
           transition: all ease .5s;
+          cursor: pointer;
         }
       }
     }
+
+    & * {
+      box-sizing: border-box;
+    }
+
+
   }
 </style>
 <template>
   <div class="advance-maker">
-    <el-form :inline="true">
-      <el-form-item label="SVG Icons:">
+    <el-form labelPosition="top">
+      <el-form-item class="icons-wrapper" label="SVG 图标:">
         <el-upload
             ref="uploader"
             action=""
@@ -60,39 +83,39 @@
         >
           <el-button><i class="el-icon-plus"></i></el-button>
         </el-upload>
+        <div class="svg-list-item" :key="i" v-for="(icon, i) in iconList">
+          <img :src="icon.url" :alt="icon.name">
+          <div class="icon-mask" @click="removeIcon(icon.uid)">
+            <i class="el-icon-delete"></i>
+          </div>
+        </div>
       </el-form-item>
-      <el-form-item label="输出目录">
+      <el-form-item label="输出到">
         <el-input v-model="outputDir" disabled>
           <el-button @click="chooseOutputDir" slot="append" icon="edit"></el-button>
         </el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" :disabled="!iconList.length || !outputDir" @click="make">生成 iconfont</el-button>
+        <el-button type="primary" :disabled="!iconList.length || !outputDir" @click="make">生成 IconFont</el-button>
       </el-form-item>
     </el-form>
-    <div class="svg-list" v-show="iconList.length">
-      <div class="svg-list-item" :key="i" v-for="(icon, i) in iconList">
-        <img :src="icon.url" :alt="icon.name">
-        <div class="icon-mask" @click="removeIcon(icon.uid)">
-          <i class="el-icon-delete"></i>
-        </div>
-      </div>
-    </div>
 
     <el-collapse accordion>
       <el-collapse-item title="高级设置">
         <el-form>
-          <el-form-item label="名称">
+          <el-form-item label="字体名称">
             <el-input v-model="options.iconfontName"></el-input>
           </el-form-item>
 
-          <el-form-item label="生成CSS">
+          <el-form-item label="生成 CSS">
             <el-switch v-model="options.css" onText="" offText=""></el-switch>
           </el-form-item>
 
-          <el-form-item label="CSS Destination" v-if="options.css">
-            <span>Custom</span>
-            <el-switch v-model="customCssDest"></el-switch>
+          <el-form-item label="CSS 输出到" v-if="options.css">
+            <div style="float: right">
+              <span>自定义</span>
+              <el-switch v-model="customCssDest"></el-switch>
+            </div>
             <el-input v-if="!customCssDest" v-model="defaultCssDest" disabled></el-input>
             <el-input v-model="options.cssDest" v-if="customCssDest">
               <el-button slot="append" icon="edit" @click="chooseCssDest"></el-button>
@@ -189,7 +212,7 @@
 			customCssDest (val) {
 				if (!val) {
 					this.options.cssDest = this.defaultCssDest
-          this.cssDir = ''
+					this.cssDir = ''
 					this.options.cssFontsPath = ''
 				}
 			}
